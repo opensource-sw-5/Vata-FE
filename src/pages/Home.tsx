9029e1c427c5ce74587d7e969e28110bc1298960
오주성 <narrnarr@dankook.ac.kr>
2025-05-14 22:30:17 +0900

    feat(#1): 프로필 생성/ 토큰 설정/ 보관함 버튼 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
-const username = localStorage.getItem("username");
+const token = localStorage.getItem("accessToken");
 
-{!username && (
+{username && (
   <div className="flex gap-8">
-    <button onClick={() => navigate("/login")} ...>로그인</button>
-    <button onClick={() => navigate("/signup")} ...>회원가입</button>
+    <button onClick={() => navigate("/storage")} ...>보관함</button>
+    <button
+      onClick={() => {
+        if (token) navigate("/input");
+        else {
+          alert("토큰 설정이 필요합니다.");
+          navigate("/token");
+        }
+      }}
+      ...
+    >
+      프로필 생성
+    </button>
+    <button onClick={() => navigate("/token")} ...>토큰 설정</button>
   </div>
 )}
