1975caf47cfb1709c07af04707e00c865ee4b143
오주성 <narrnarr@dankook.ac.kr>
2025-05-16 22:24:10 +0900

    feat(#2): 회원가입 화면 기본 UI

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
