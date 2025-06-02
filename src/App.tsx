9b337ea0f2c5eb22c17d59ac45ffdccae9caa8d8
jsgo53 <bjchi@naver.com>
2025-06-02 11:28:46 +0900

    chore(#20): API 인증 구조 개선 및 불필요한 라우팅 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import AccessToken from "./pages/AccessToken";
 import Input from "./pages/Input";
 import Result from "./pages/Result";
-import Storage from "./pages/Storage"; // 보관함 import 추가
+import Storage from "./pages/Storage";
 
 function App() {
   return (
@@ -14,10 +13,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/token" element={<AccessToken />} />
         <Route path="/input" element={<Input />} />
         <Route path="/result" element={<Result />} />
-        <Route path="/storage" element={<Storage />} /> {/* 보관함 라우팅 추가 */}
+        <Route path="/storage" element={<Storage />} />
       </Routes>
     </Router>
   );
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,24 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://vata.kro.kr:8080", // 실제 서버 URL
   withCredentials: true,
-});
-
-// 인터셉터 설정
-instance.interceptors.request.use((config) => {
-  const skipAuthUrls = ["/api/auth/signup", "/api/auth/login", "/api/auth/token"];
-  const isSkip = skipAuthUrls.some((url) => config.url?.includes(url));
-
-  if (!isSkip) {
-    const token = localStorage.getItem("accessToken") || sessionStorage.getItem("accessToken");
-    if (token && config.headers) {
-      // 문자열 템플릿 리터럴을 사용하여 Authorization 헤더에 토큰 추가
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-
-  return config;
 });
 
 export default instance;
